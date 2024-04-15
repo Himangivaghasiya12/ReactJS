dcb4c472562117f1a8028e5156972c38a0e28206
Himangivaghasiya12 <gopivaghasiya001@gmail.com>
2024-04-15 15:34:12 +0530

    15/04/2024

---
+++ JAVASCRIPT/PROJECT/EXAMPLE/style.js
@@ -0,0 +1,88 @@
+document.getElementsByClassName("index").innerHTML
+= "hello good morning"
+
+document.getElementsByTagName("p").innerHTML
+= "hello "
+
+// 14.
+{
+
+    let array=["H" ,"I" ,"M","A","N","G","I"]
+    console.log(array);
+    
+    let reverce=array.reverse()
+    
+        console.log(reverce);
+    
+}
+
+// 15.
+{
+
+    let array1=["skill"]
+    let array2=["qode"]
+    
+    let newarray=array1.concat(array2)
+    
+    console.log(newarray);
+}
+
+// 16.
+
+// let original = "Hello World";
+// let reversed = "";
+// for (let i = original.length - 1; i >= 0; i--) {
+//     reversed += original[i];
+// }
+// console.log(reversed);
+
+// 19.
+
+// let array=[1,2,3,4];
+
+// array[2]=8;
+
+// console.log(array);
+
+
+// 39.
+
+// let date1 = new Date()
+// console.log(date1);
+
+
+// 54.
+// break
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         break; 
+//     }
+//     console.log(i);
+// }
+// // continue
+// for (let i = 0; i < 5; i++) {
+//     if (i === 2) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 55.
+// let mark = 20;
+// let result = mark <= 80 ? "Good" : "Excellent";
+// console.log(result); 
+
+// 4.
+{
+//     let age = 18;
+
+// if(age>=18)
+// {
+//     console.log("Eligible for Voting!");
+// }
+// else{
+//     console.log("Not Eligible for Voting!");
+// }
+
+
+}
--- JAVASCRIPT/PROJECT/LECTURE_6/P_3.js
+++ JAVASCRIPT/PROJECT/LECTURE_6/P_3.js
@@ -79,9 +79,9 @@
 // Calculate the sum of all the numbers in the following array
 // var numbersArray = [1,13,22,123,49]
 
-var numbersArray = [1, 13, 22, 123, 49];
-var sum = 0;
-for (var i = 0; i < numbersArray.length; i++) {
-  sum = sum + numbersArray[i];
-}
-console.log("The sum is: " + sum);
+// var numbersArray = [1, 13, 22, 123, 49];
+// var sum = 0;
+// for (var i = 0; i < numbersArray.length; i++) {
+//   sum = sum + numbersArray[i];
+// }
+// console.log("The sum is: " + sum);
